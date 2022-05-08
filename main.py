42fd328e35af1d5dc367ab091e0f3a893ad693e4
Lalit Shrotriya <81531821+Shrotriya-lalit@users.noreply.github.com>
2022-05-08 16:00:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':  # Execute the following code only when executing main.py (not when importing it)
 
-    binance = BinanceClient("b5d72a9ec2e1106eb0135c70e93664260aa319c20354a908bbbcded25767e0c8",
-                            "61439a1ed457ea466d1382f08c3ba2f56ea251bdb9e6990c0e406b176d85dce2",
+    binance = BinanceClient("API KEY",
+                            "API SECRET KEY",
                             testnet=True, futures=True)
-    bitmex = BitmexClient("OPC2fOgpjfVggO-BT7fUIU-4", "xF9bbif2_v4FLjpEsZaqyWhj8HM2J-zDGxtQ32G4kyI5vfH8", testnet=True)
+    bitmex = BitmexClient("API KEY", "API SECRET KEY", testnet=True)
 
     root = Root(binance, bitmex)
     root.mainloop()
